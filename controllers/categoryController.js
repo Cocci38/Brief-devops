7b84869a8b29d9315cb6089a7f3d668263c03470
Cocci38 <sandra.villot038@gmail.com>
2023-04-13 14:08:43 +0200

    findById, update, delete category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,10 +1,32 @@
 import Category from "../models/Category.js";
 
 // Get All Categories
-export const getCategories = (req, res, next) => {
-    res.render("category/getCategories", {
+export const getCategories = async (req, res, next) => {
+    // Création d'un nouvelle catégorie
+    const categories = await Category.find();
+    console.log(categories);
+    //res.json(categories);
+    res.status(200).render('category/getCategories', {
         title: "CategoryList",
+        categories: categories,
     });
+
+};
+
+// Get Categorie par l'id
+export const getCategory = async (req, res, next) => {
+
+    console.log(req.path.split("/")[2]);
+    // Création d'un nouvelle catégorie
+    const category = await Category.findById(req.path.split("/")[2]);
+
+    console.log(category);
+    //res.json(category);
+    res.status(200).render('category/getCategory', {
+        title: "CategoryOne",
+        category: category,
+    });
+
 };
 
 // Création des catégories
@@ -18,5 +40,34 @@ export const postCategory = async (req, res, next) => {
 
     console.log(category);
     //res.status(201).redirect("/categories");
+    res.status(201).json({ category });
+};
+
+// Création des catégories
+export const putCategory = async (req, res, next) => {
+    const _id = req.body._id;
+    const categoryName = req.body.categoryName;
+
+    // Création d'un nouvelle catégorie
+    const category = await Category.findByIdAndUpdate({
+        _id,
+    }, {
+        categoryName,
+    }, {
+        new: true,
+    });
+
+    console.log(category);
+    //res.status(201).redirect("/categories");
+    res.status(201).json({ category });
+};
+
+export const deleteCategory = async (req, res, next) => {
+    const _id = req.body._id;
+
+    const category = await Category.findByIdAndDelete({
+        _id
+    });
+    console.log(category);
     res.status(201).json({ category });
 };
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,13 +1,22 @@
 import express from "express";
 import {
     getCategories,
+    getCategory,
     postCategory,
+    putCategory,
+    deleteCategory,
 } from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.get("/categories", getCategories);
 
+router.get("/category/:_id", getCategory);
+
 router.post("/create-category", postCategory);
+
+router.put("/update-category", putCategory);
+
+router.delete("/delete-category", deleteCategory);
 
 export default router;
